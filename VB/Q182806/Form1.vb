9e38cf5dc8f75318d92c373ec4043011db50953d
DevExpressExampleBot <fakeEmail>
2019-02-16 00:26:56 +0300

    Source auto update [skip ci]

--- VB/Q182806/Form1.vb
+++ VB/Q182806/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -8,49 +7,51 @@ Imports System.Text
 Imports System.Windows.Forms
 
 Namespace Q182806
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-			Dim dataSet11 As New DataSet()
-			dataSet11.Tables.Add(GetCustomerDataTable())
-			dataSet11.Tables.Add(GetPersonDataTable())
-			Dim keyColumn As DataColumn = dataSet11.Tables("Customers").Columns("ID")
-			Dim foreignKeyColumn As DataColumn = dataSet11.Tables("Persons").Columns("ID")
-			dataSet11.Relations.Add("CustomersPersons", keyColumn, foreignKeyColumn)
-			myGridControl1.DataSource = dataSet11.Tables("Customers")
-			myGridView1.KeyFieldName = "Items"
-			myGridControl1.ForceInitialize()
-		End Sub
-		Private Function GetCustomerDataTable() As DataTable
-			Dim table As New DataTable()
-			table.TableName = "Customers"
-			table.Columns.Add(New DataColumn("Items", GetType(String)))
-			table.Columns.Add(New DataColumn("Money", GetType(Double)))
-			table.Columns.Add(New DataColumn("ID", GetType(Integer)))
-			For i As Integer = 0 To 9
-				table.Rows.Add("Product " & i, 3000 + i * 298.55D, i)
-			Next i
-			Return table
-		End Function
-		Private Function GetPersonDataTable() As DataTable
-			Dim table As New DataTable()
-			table.TableName = "Persons"
-			table.Columns.Add(New DataColumn("FirstName", GetType(String)))
-			table.Columns.Add(New DataColumn("SecondName", GetType(String)))
-			table.Columns.Add(New DataColumn("Age", GetType(Integer)))
-			table.Columns.Add(New DataColumn("ID", GetType(Integer)))
-			For i As Integer = 0 To 49
-				Dim name As String = "Adam"
-				Dim secondName As String = "Smith"
-				If i Mod 2 = 0 Then
-					name = "Ben"
-					secondName = "Black"
-				End If
-				table.Rows.Add(name, secondName, 20 + i \ 2, i Mod 10)
-			Next i
-			Return table
-		End Function
-	End Class
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+            Dim dataSet11 As New DataSet()
+            dataSet11.Tables.Add(GetCustomerDataTable())
+            dataSet11.Tables.Add(GetPersonDataTable())
+            Dim keyColumn As DataColumn = dataSet11.Tables("Customers").Columns("ID")
+            Dim foreignKeyColumn As DataColumn = dataSet11.Tables("Persons").Columns("ID")
+            dataSet11.Relations.Add("CustomersPersons", keyColumn, foreignKeyColumn)
+            myGridControl1.DataSource = dataSet11.Tables("Customers")
+            myGridView1.KeyFieldName = "Items"
+            myGridControl1.ForceInitialize()
+        End Sub
+        Private Function GetCustomerDataTable() As DataTable
+            Dim table As New DataTable()
+            table.TableName = "Customers"
+            table.Columns.Add(New DataColumn("Items", GetType(String)))
+            table.Columns.Add(New DataColumn("Money", GetType(Double)))
+            table.Columns.Add(New DataColumn("ID", GetType(Integer)))
+            For i As Integer = 0 To 9
+                table.Rows.Add("Product " & i, 3000 + i * 298.55D, i)
+            Next i
+            Return table
+        End Function
+        Private Function GetPersonDataTable() As DataTable
+            Dim table As New DataTable()
+            table.TableName = "Persons"
+            table.Columns.Add(New DataColumn("FirstName", GetType(String)))
+            table.Columns.Add(New DataColumn("SecondName", GetType(String)))
+            table.Columns.Add(New DataColumn("Age", GetType(Integer)))
+            table.Columns.Add(New DataColumn("ID", GetType(Integer)))
+            For i As Integer = 0 To 49
+
+                Dim name_Renamed As String = "Adam"
+                Dim secondName As String = "Smith"
+                If i Mod 2 = 0 Then
+                    name_Renamed = "Ben"
+                    secondName = "Black"
+                End If
+                table.Rows.Add(name_Renamed, secondName, 20 + i \ 2, i Mod 10)
+            Next i
+            Return table
+        End Function
+    End Class
 
 End Namespace
--- VB/Q182806/MyGridView.vb
+++ VB/Q182806/MyGridView.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Text
 Imports DevExpress.XtraGrid.Views.Grid
@@ -12,101 +11,105 @@ Imports System.Reflection
 Imports DevExpress.XtraEditors
 
 Namespace Q182806
-	Public Class MyGridView
-		Inherits GridView
-		Public Sub New(ByVal control As GridControl)
-			MyBase.New(control)
-		End Sub
+    Public Class MyGridView
+        Inherits GridView
 
-		Public Sub New()
-			Me.New(Nothing)
-		End Sub
+        Public Sub New(ByVal control As GridControl)
+            MyBase.New(control)
+        End Sub
 
-		Protected Overrides ReadOnly Property ViewName() As String
-			Get
-				Return "MyGridView"
-			End Get
-		End Property
+        Public Sub New()
+            Me.New(Nothing)
+        End Sub
 
-		Private keyFieldName_Renamed As String
+        Protected Overrides ReadOnly Property ViewName() As String
+            Get
+                Return "MyGridView"
+            End Get
+        End Property
 
-		<Category(CategoryName.Data), DefaultValue(""), TypeConverter("System.Windows.Forms.Design.DataMemberFieldConverter, System.Design"), Editor("System.Windows.Forms.Design.DataMemberFieldEditor, System.Design", GetType(System.Drawing.Design.UITypeEditor))> _
-		Public Property KeyFieldName() As String
-			Get
-				Return keyFieldName_Renamed
-			End Get
-			Set(ByVal value As String)
-				keyFieldName_Renamed = value
-			End Set
-		End Property
 
-		Protected Overrides Sub PopulateTabMasterData(ByVal tabControl As ViewTab, ByVal rowHandle As Integer)
-			tabControl.Pages.Clear()
-			MyBase.PopulateTabMasterData(tabControl, rowHandle)
-			If keyFieldName_Renamed = String.Empty Then
-				Return
-			End If
-			If GridControl.DefaultView.IsZoomedView Then
-				ExtendTabCaption(tabControl,keyFieldName_Renamed, True)
-			End If
-		End Sub
+        Private keyFieldName_Renamed As String
 
-		Private Sub ExtendTabCaption(ByVal tabControl As ViewTab, ByVal keyField As String, ByVal isForZoomed As Boolean)
-			For Each page As ViewTabPage In tabControl.Pages
-				If (Not isForZoomed) AndAlso page.Text.Contains(" for ") Then
-					page.Text = page.Text.Remove(page.Text.IndexOf(" for ", 0))
-				Else
-					Dim view As MyGridView = CType(GridControl.FocusedView, MyGridView)
-					page.Text &= " for " & view.GetDataRow(0).GetParentRow(view.LevelName)(keyField).ToString()
-				End If
-			Next page
-		End Sub
+        <Category(CategoryName.Data), DefaultValue(""), TypeConverter("System.Windows.Forms.Design.DataMemberFieldConverter, System.Design"), Editor("System.Windows.Forms.Design.DataMemberFieldEditor, System.Design", GetType(System.Drawing.Design.UITypeEditor))> _
+        Public Property KeyFieldName() As String
+            Get
+                Return keyFieldName_Renamed
+            End Get
+            Set(ByVal value As String)
+                keyFieldName_Renamed = value
+            End Set
+        End Property
 
-		Protected Overrides Sub ZoomView(ByVal prevView As BaseView)
-			MyBase.ZoomView(prevView)
-			CType(ParentView, MyGridView).MyPopulateTabMasterData(True, Me)
-			TabControl.LayoutChanged()
-		End Sub
+        Protected Overrides Sub PopulateTabMasterData(ByVal tabControl As ViewTab, ByVal rowHandle As Integer)
+            tabControl.Pages.Clear()
+            MyBase.PopulateTabMasterData(tabControl, rowHandle)
+            If keyFieldName_Renamed = String.Empty Then
+                Return
+            End If
+            If GridControl.DefaultView.IsZoomedView Then
+                ExtendTabCaption(tabControl,keyFieldName_Renamed, True)
+            End If
+        End Sub
 
-		Public Sub MyPopulateTabMasterData(ByVal isViewZoomed As Boolean, ByVal view As BaseView)
-			Dim fi As FieldInfo = GetType(BaseView).GetField("tabControl", BindingFlags.Instance Or BindingFlags.NonPublic)
-			Dim tab As ViewTab = CType(fi.GetValue(view), ViewTab)
-			ExtendTabCaption(tab, CStr((CType(GridControl.MainView, MyGridView)).keyFieldName), isViewZoomed)
-		End Sub
+        Private Sub ExtendTabCaption(ByVal tabControl As ViewTab, ByVal keyField As String, ByVal isForZoomed As Boolean)
+            For Each page As ViewTabPage In tabControl.Pages
+                If Not isForZoomed AndAlso page.Text.Contains(" for ") Then
+                    page.Text = page.Text.Remove(page.Text.IndexOf(" for ", 0))
+                Else
+                    Dim view As MyGridView = CType(GridControl.FocusedView, MyGridView)
+                    page.Text &= " for " & view.GetDataRow(0).GetParentRow(view.LevelName)(keyField).ToString()
+                End If
+            Next page
+        End Sub
 
-		Public Overrides Sub NormalView()
-			MyBase.NormalView()
-			CType(ParentView, MyGridView).MyPopulateTabMasterData(False, Me)
-			TabControl.LayoutChanged()
-		End Sub
-	End Class
+        Protected Overrides Sub ZoomView(ByVal prevView As BaseView)
+            MyBase.ZoomView(prevView)
+            CType(ParentView, MyGridView).MyPopulateTabMasterData(True, Me)
+            TabControl.LayoutChanged()
+        End Sub
 
-	Public Class MyGridInfoRegistrator
-		Inherits GridInfoRegistrator
-		Public Sub New()
-			MyBase.New()
-		End Sub
+        Public Sub MyPopulateTabMasterData(ByVal isViewZoomed As Boolean, ByVal view As BaseView)
+            Dim fi As FieldInfo = GetType(BaseView).GetField("tabControl", BindingFlags.Instance Or BindingFlags.NonPublic)
+            Dim tab As ViewTab = DirectCast(fi.GetValue(view), ViewTab)
+            ExtendTabCaption(tab, CStr(CType(GridControl.MainView, MyGridView).keyFieldName_Renamed), isViewZoomed)
+        End Sub
 
-		Public Overrides ReadOnly Property ViewName() As String
-			Get
-				Return "MyGridView"
-			End Get
-		End Property
+        Public Overrides Sub NormalView()
+            MyBase.NormalView()
+            CType(ParentView, MyGridView).MyPopulateTabMasterData(False, Me)
+            TabControl.LayoutChanged()
+        End Sub
+    End Class
 
-		Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
-			Return New MyGridView(grid)
-		End Function
-	End Class
+    Public Class MyGridInfoRegistrator
+        Inherits GridInfoRegistrator
 
-	Public Class MyGridControl
-		Inherits GridControl
-		Public Sub New()
-			MyBase.New()
-		End Sub
+        Public Sub New()
+            MyBase.New()
+        End Sub
 
-		Protected Overrides Sub RegisterAvailableViewsCore(ByVal collection As InfoCollection)
-			MyBase.RegisterAvailableViewsCore(collection)
-			collection.Add(New MyGridInfoRegistrator())
-		End Sub
-	End Class
+        Public Overrides ReadOnly Property ViewName() As String
+            Get
+                Return "MyGridView"
+            End Get
+        End Property
+
+        Public Overrides Function CreateView(ByVal grid As GridControl) As BaseView
+            Return New MyGridView(grid)
+        End Function
+    End Class
+
+    Public Class MyGridControl
+        Inherits GridControl
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+        Protected Overrides Sub RegisterAvailableViewsCore(ByVal collection As InfoCollection)
+            MyBase.RegisterAvailableViewsCore(collection)
+            collection.Add(New MyGridInfoRegistrator())
+        End Sub
+    End Class
 End Namespace
